fa46b129a7a4eb14c4d4c5dbff8899ef13d34084
Mauro Valencia <andres228cv@gmail.com>
2022-09-28 11:46:57 -0500

    Project & Client queries

---
+++ server/models/Client.js
@@ -0,0 +1,15 @@
+// Mongoose Models
+const mongoose = require("mongoose");
+
+const ClientSchema = new mongoose.Schema({
+  name: {
+    type: String,
+  },
+  email: {
+    type: String,
+  },
+  phone: {
+    type: String,
+  },
+});
+module.exports = mongoose.model("Client", ClientSchema);
